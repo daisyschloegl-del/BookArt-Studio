5c6f7c6d73ca4b2ff47c308d7600a8105d62993d
daisyschloegl-del <daisy.schloegl@gmail.com>
2026-08-04 00:22:48 +0200

    Update image.js

--- js/image.js
+++ js/image.js
@@ -1,1 +1,10 @@
+export function loadImage(file) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
 
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+
+    img.src = URL.createObjectURL(file);
+  });
+}
