96cd1b7b9cd8c7408438a546501ad20c2dee847c
daisyschloegl-del <daisy.schloegl@gmail.com>
2026-08-03 21:16:33 +0200

    Update generator.js

--- js/js/generator.js
+++ js/js/generator.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("BookArt Studio gestartet!");
+    const input = document.getElementById("imageInput");
+    const preview = document.getElementById("preview");
+
+    input.addEventListener("change", (event) => {
+        const file = event.target.files[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+            preview.style.display = "block";
+        };
+
+        reader.readAsDataURL(file);
+    });
 });
