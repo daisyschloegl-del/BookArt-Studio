58bc3572a3d4f1ee2635f62bef7167506f826b97
daisyschloegl-del <daisy.schloegl@gmail.com>
2026-08-03 19:25:29 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML += `
+    <div style="margin:20px;padding:20px;border:1px solid #ccc;">
+      <h2>BookArt Studio</h2>
+      <p>Der Generator wurde erfolgreich gestartet.</p>
+    </div>
+  `;
+});
