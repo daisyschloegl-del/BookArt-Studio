7dc92b997a05285af136200cf6c85b2631935f32
daisyschloegl-del <daisy.schloegl@gmail.com>
2026-08-03 21:32:56 +0200

    Update generator.js

--- js/js/generator.js
+++ js/js/generator.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const input = document.getElementById("imageInput");
-    const preview = document.getElementById("preview");
 
-    input.addEventListener("change", (event) => {
-        const file = event.target.files[0];
-        if (!file) return;
-
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-            preview.src = e.target.result;
-            preview.style.display = "block";
-        };
-
-        reader.readAsDataURL(file);
-    });
-});
