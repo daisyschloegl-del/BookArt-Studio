0b1de8b06e583ef2b459b7760c27585a6e4b56c1
daisyschloegl-del <daisy.schloegl@gmail.com>
2026-08-04 18:28:12 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,56 +1,26 @@
 
-import { generatePattern } from "./generator.js";
-import { exportPDF } from "./pdf.js";
-document.addEventListener("DOMContentLoaded", () => {
-
-    const input = document.getElementById("imageInput");
-    const preview = document.getElementById("preview");
-    const button = document.getElementById("generateBtn");
-
-    input.addEventListener("change", () => {
-        const file = input.files[0];
-        if (!file) return;
-
-        const reader = new FileReader();
-
-        reader.onload = e => {
-            preview.src = e.target.result;
-            preview.style.display = "block";
-        };
-
-        reader.readAsDataURL(file);
-    });
-
-    button.addEventListener("click", () => {
-
-        
-
-        let result;
-
-if (preview.complete && preview.naturalWidth) {
-    result = generatePattern(preview, 365);
-} else {
-    result = generateTextPattern();
-}
-
-        let text = "Seite | Start | Ende\n";
-        text += "--------------------\n";
-
-        result.forEach(r => {
-            text += `${r.page} | ${r.start} | ${r.end}\n`;
-        });
-
-        exportPDF(text);
-    });
-
-});
-function generateTextPattern() {
+   function generateTextPattern() {
     const textInput = document.getElementById("textInput").value;
+    const font = document.getElementById("fontSelect").value;
 
-    alert("Text erkannt: " + textInput);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
 
-    return [
-        { page: 1, start: 40, end: 120 }
-    ];
-}
-    
+    canvas.width = 400;
+    canvas.height = 200;
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = `80px ${font}`;
+
+    ctx.fillText(textInput, canvas.width / 2, canvas.height / 2);
+
+    const img = new Image();
+    img.src = canvas.toDataURL();
+
+    return generatePattern(img, 365);
+} 
