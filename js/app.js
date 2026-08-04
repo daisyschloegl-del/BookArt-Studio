1efc55e42c4399988ed1f319ef465479e6af48c6
daisyschloegl-del <daisy.schloegl@gmail.com>
2026-08-04 15:23:35 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -45,16 +45,12 @@ if (preview.complete && preview.naturalWidth) {
 
 });
 function generateTextPattern() {
+    const textInput = document.getElementById("textInput").value;
+
+    alert("Text erkannt: " + textInput);
+
     return [
-        { page: 1, start: 40, end: 120 },
-        { page: 2, start: 38, end: 122 },
-        { page: 3, start: 36, end: 124 },
-        { page: 4, start: 34, end: 126 },
-        { page: 5, start: 32, end: 128 },
-        { page: 6, start: 30, end: 130 },
-        { page: 7, start: 28, end: 132 },
-        { page: 8, start: 26, end: 134 },
-        { page: 9, start: 24, end: 136 },
-        { page: 10, start: 22, end: 138 }
+        { page: 1, start: 40, end: 120 }
     ];
 }
+    
